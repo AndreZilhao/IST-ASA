d1511e80d5e6897521e9079e75e15b8de1644f8f
AndreZilhao-HP <aalmsantos@gmail.com>
2015-03-23 20:23:08 +0000

    versao final comentada

--- Proj1/Proj1.cpp
+++ Proj1/Proj1.cpp
@@ -1,12 +1,18 @@
+ /* Andre Zilhao @ ist.utl.pt
+ Projecto de Algoritmos e Sintese de Dados - Parte 1
+ March 23 2015
+ Description: Program that given an input, finds the distance between point 'Erdos' and all the nodes in a graph
+ and returns the longest distance, followed by a list of ocurrences for every distance shorter than the longest.
+ */
 #include <iostream>
 #include <list>
 using namespace std;
- 
+
 // Classe Grafo com constructor, adição de Ramos (informação guardada no grafo), e algoritmo de pesquisa.
 class Grafo
 {
 	int V; // Numero de Vertices.
-	list<int> *adj;    // Lista de Adjacentes.
+	std::list<int> *adj;    // Lista de Adjacentes.
 public:
  	Grafo(int V);  
 	void novoRamo(int v, int w); // funcao para adicionar ramos.
@@ -32,20 +38,18 @@ void Grafo::findErdos(int s)
 	bool *visited = new bool[V];
 	int *weight = new int[V];
 	int maxWeight = 0;
-	//int temp = 0;
-	int w2[V];
+	int arrayPesos[V];
 	for(int i = 0; i < V; i++)
 		 {
-		 	w2[i] = 0;
+		 	arrayPesos[i] = 0;
 		 }
 	//Coloca pesos de viagem a 0, e marca todos os colaboradores como nao visitados.
 	for(int i = 0; i < V; i++)
 		visited[i] = false;
 	for(int i = 0; i < V; i++)
 		weight[i] = 0;
-	// Criar a queue e a lista de pesos de viagem.
+	// Criar a queue
 	list<int> queue;
-	//list<int> weightSum;
 	// Caso 1
 	visited[v] = true;
 	queue.push_back(v);
@@ -59,50 +63,30 @@ void Grafo::findErdos(int s)
 		queue.pop_front();
 		//Percore todas as adjacencias de um Colaborador. Se um colaborador nao foi visitado, assinalar como tal e
 		//assinalar-lhe um peso. Se o peso assinalado for o mais alto, criar um novo elemento na lista de pesos.
-		//Por fim, incrementar a ultima posicao da lista de pesos com a ocorrencia.
+		//Por fim, incrementar a posicao da array de pesos com a ocorrencia.
 		for(i = adj[v].begin(); i != adj[v].end(); ++i)
 		{
 			if(!visited[*i])
 			{
-				/*if (weight[s]+1>maxWeight)
-					 {
-						maxWeight = weight[s]+1;
-						weightSum.push_back(0);
-
-					 }*/
 				visited[*i] = true;
 				weight[*i] = weight[v]+1;
 				queue.push_back(*i);
-				if (w2[weight[v]+1] == 0)
+				if (arrayPesos[weight[v]+1] == 0)
 				{
 					maxWeight++;
 				}
-				w2[weight[v]+1]++;
-				
-				/*temp = weightSum.back();
-				weightSum.pop_back();
-				weightSum.push_back(temp+1);*/
-				
-
-
+				arrayPesos[weight[v]+1]++;
 			}
 		}
 	}
 	// Print de "M"
-	//cout << maxWeight << "\n";
-	/*
-	while(!weightSum.empty())
-	{
-		//Print de M linhas [1..M]
-		cout << weightSum.front() << "\n";
-		weightSum.pop_front();
-	} */
 	cout << maxWeight << "\n";
 	for(int i = 0; i < V; i++)
 		 {
-		 	if (w2[i] != 0) 
+		 	if (arrayPesos[i] != 0) 
 		 		{
-		 			cout << w2[i] << "\n";
+		 			//Print de M linhas [1..M]
+		 			cout << arrayPesos[i] << "\n";
 
 		 		}
 		 	else
