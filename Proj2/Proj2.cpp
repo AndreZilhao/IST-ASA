052ec7a6336ddc40dc5cecf3c2a19f11d594d852
AndreZilhao-HP <aalmsantos@gmail.com>
2015-04-17 09:35:07 +0100

    Versao inicial com erros de answer

--- Proj2/Proj2.cpp
+++ Proj2/Proj2.cpp
@@ -19,7 +19,11 @@ p - Cost of Branch.
 */
 #include <iostream>
 #include <list>
+#include <limits.h>
+#define INFINITOSUP INT_MAX
 using namespace std;
+
+
 
 // Classe Grafo com constructor, adição de Ramos (informação guardada no grafo), e algoritmo de pesquisa.
 
@@ -55,7 +59,9 @@ public:
  	Grafo(int V);  
 	void novoArco(int v, int w, int peso);		 // Funcao para adicionar ramos ao grafo. (Vertice origem, Vertice Destino, Peso)
 	void analiseCustos(int s); 			 // Algoritmo de analise do problema. (Belman-Ford Modificado)
-	void traceGrafo(); 				 // Função de Print do grafo para testes.
+	void traceGrafo(int array[], int source, char negCheck[]);			         // Função de Print do grafo para testes.
+	void relaxGrafo(int array[]);
+	void negativeCycleCheck(int array[], int source);
 
 };
   
@@ -68,22 +74,103 @@ public:
  							 // Adicionar novo ramo entre 2 colaboradores, para um grafo direcional.
 void Grafo::novoArco(int v, int u, int peso)
 {
-	Arco a(u-1, peso);
-	int tst = a.getPeso();
-	adj[v-1].push_back(a);
+	Arco a(u, peso);
+	adj[v].push_back(a);
 }
  
-void Grafo::analiseCustos(int s)
-{//TODO
+void Grafo::analiseCustos(int source)
+{
+	//Passos de Inicialização Belman-Ford
+	int arrayPesos[V];
+	//int flagOptimizacao = false; 
+	for(int i = 0; i < V; i++)
+	{
+		arrayPesos[i] = INFINITOSUP;
+	}
+	arrayPesos[source] = 0;
+	//
+	for(int i = 0; i < V; i++)
+	{
+		this->relaxGrafo(arrayPesos);
+	}
+	this->negativeCycleCheck(arrayPesos, source);
+	//this->traceGrafo(arrayPesos,source);
+	
 }
 
-void Grafo::traceGrafo()
+void Grafo::relaxGrafo(int array[])
+{
+	list<Arco>::iterator it;
+	for(int i = 0; i < V; i++)
+	{
+		if (array[i] != INFINITOSUP)
+		{
+			{	
+				for (it = adj[i].begin(); it != adj[i].end(); it++)
+				{
+					if (array[it->getVertice()] > array[i] + it->getPeso())
+					{
+						array[it->getVertice()] = array[i] + it->getPeso();
+					}
+				}
+			}
+		}
+	}
+}
+
+void Grafo::negativeCycleCheck(int array[], int source)
+{//TODO
+	char negCheck[V];
+	int backupArray[V];
+      	for (int i = 0; i < V; i++)
+		backupArray[i] = array[i];
+	list<Arco>::iterator it;
+	for (int j = 0; j < V; j++)
+	{
+		for (int i = 0; i < V; i++)
+		{
+			if (array[i]== INFINITOSUP) 
+			{
+				negCheck[i] = 'U';
+			} else 
+			{
+				negCheck[i] = 'V';
+			}
+		
+			for (it = adj[i].begin(); it != adj[i].end(); it++)
+			{
+				if (array[it->getVertice()] > array[i] + it->getPeso())
+				{
+					array[it->getVertice()] = array[i] + it->getPeso();
+				}
+			}
+		}
+		for (int i = 0; i < V; i++)
+		{
+			if (backupArray[i] != array[i])
+			{
+				negCheck[i] = 'I';
+			}
+		}
+	}
+	this->traceGrafo(backupArray, source, negCheck);
+}
+
+void Grafo::traceGrafo(int array[], int source, char negCheck[])
 {
 	for(int i = 0; i < V; i++)
 		 {
-		 	if (true)			 // IMPORTANTE: Adicionar possivel verificação para casos nulos ou remover 'if'.
+		 	if (negCheck[i] == 'V')			 // IMPORTANTE: Adicionar possivel verificação para casos nulos ou remover 'if'.
 		 	{	
-				cout << "___VERTICE: " << i+1 << "\n";
+				cout << array[i] << "\n";
+			} else 
+			{
+				cout << negCheck[i] << "\n";
+			}
+
+
+				
+			/*	cout << "___VERTICE: " << i+1 << "\n";
 		 		list<Arco> queue = adj[i];
 				while(!queue.empty())
 				{
@@ -91,10 +178,12 @@ void Grafo::traceGrafo()
 					cout << "Peso Filho: " << queue.front().getPeso() << "\n";
 					queue.pop_front();
 				}
+				cout << "Distancia da source: " << array[i] << "\n";
 				
 		 	}
 		 	else
 		 		{}
+				*/
 		 }
 
 }
@@ -110,10 +199,11 @@ int main()
 		while(nCustos>0)
 		{
 			cin >> w >> v >> peso;
-			localidades.novoArco(w, v, peso);
+			localidades.novoArco(w-1, v-1, peso);
 			--nCustos;
 		}
 	}
-	localidades.traceGrafo();			 //Analise de custos (Bellman-Ford modificado) apartir da sede de empresa.
-	return 0;
+	localidades.analiseCustos(sedeEmpresa-1);
+				 //Analise de custos (Bellman-Ford modificado) apartir da sede de empresa.
+return 0;
 }
